8aefaf999cda5959231ea402e43c6ccef60c88c1
vucms <vuzero007@gmail.com>
2023-12-10 12:40:11 +0700

    fix: remove password from response user object

--- models/user.model.js
+++ models/user.model.js
@@ -18,6 +18,7 @@ module.exports.User = {
               ? {
                   ...row,
                   verified: row.verified === 1,
+                  password: undefined,
                 }
               : row
           );
